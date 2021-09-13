bc17f8dc39bad98523ce38660868012fa27c6b12
Leandro <officialdevmi@gmail.com>
2021-09-13 02:59:25 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navbar from './components/Navbar';
+import Slogan from './components/Slogan';
 
 const App = () => (
   <div>
     <Navbar />
+    <Slogan />
   </div>
 );
 
---
+++ src/components/Slogan.js
@@ -0,0 +1,10 @@
+import './Slogan.css';
+
+const Slogan = () => (
+  <div className="Slogan-Container">
+    <span className="Slogan-Text">Coin shopping.</span>
+    <span className="Slogan-Text Simple">Made simple.</span>
+  </div>
+);
+
+export default Slogan;
